646f605e22a811a8d1168fc263514b1c548f8273
Morgan Newman <mpnewman95@gmail.com>
2018-11-07 13:27:45 -0800

    Solves max-sum-in-array

--- 4-max-sum-in-the-array.js
+++ 4-max-sum-in-the-array.js
@@ -1,49 +1,48 @@
-//input [4,6,-3,5,-2,1]
-// expected output 12
-
-function maxSum(arr) {
-  for (let i = 0; i < arr.length; i++) {}
-
-  return largestSum;
-}
+// 1. Go through a simple example with no algorithm per se
 
 // arr[1, -2, 2]
 // 1
 // 1, -2
 // 1, -2, 2
 // -2, 2
+// -2
 // 2
 
-/*
-maxSum(arr){
-  max = null 
-  follower = [0]
+// 2. Come up with an algorithm to explain your naive solution
 
-  while (array[follower]) {
-    running total = arr[follower] 
-    leader equal to follower + 1 
-    
-    while( array[leader])
-      
-
-
-
-
-
-  }
-
-
-
-  return max
-*/
-
-// keep running total of max
+// keep runningTotal of max
 // while follower is not null
 // first element
 // follower at first element, leader at follower + 1
 // while leader is not null, move leader forward +1
-//    add leader current value to running total
+//    add leader current value to runningTotal
 
-// if running total > max, max = running total
+// if runningTotal > max, max = runningTotal
 // move follower forward +1
 // leader = follower + 1
+
+// 3. Try and come up with code to fit your "English" algorithm
+function maxSum(arr) {
+  let max = arr[0];
+  let follower = 0;
+  let leader;
+  let runningTotal;
+  while (arr[follower] !== undefined) {
+    runningTotal = arr[follower];
+    // console.log(runningTotal);
+    if (runningTotal > max) max = runningTotal;
+    leader = follower + 1;
+
+    while (arr[leader] !== undefined) {
+      runningTotal += arr[leader];
+      if (runningTotal > max) max = runningTotal;
+      leader++;
+    }
+
+    follower++;
+  }
+  return max;
+}
+
+// console.log(maxSum([1, -2, 2])); // 2
+console.log(maxSum([0, 0, -3, 5, -2, 1])); // 12
