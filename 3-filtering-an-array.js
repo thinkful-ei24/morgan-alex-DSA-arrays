1b931f29509fca4f73ecf992036ed60040fee141
Morgan Newman <mpnewman95@gmail.com>
2018-11-07 12:26:23 -0800

    Adds second version of filter problem

--- 3-filtering-an-array.js
+++ 3-filtering-an-array.js
@@ -1,12 +1,32 @@
+// Returning a new array
 const filter = (arr, callback) => {
   const result = [];
   for (let i = 0; i < arr.length; i++) {
-    if (callback(arr[i])) {
+    if (callback(arr[i], i, arr)) {
       result.push(arr[i]);
     }
   }
   return result;
 };
-console.log(filter([1, 2, 3, 4], num => num > 2)); //  [3, 4]
-//input array
-//return array all numbers greater than 5
+// console.log(filter([2, 7, 5, 3, 7], num => num >= 5)); //  [7, 5, 7]
+
+// Returning the same array
+const filterRedux = (arr, callback) => {
+  function removeItem(arr, i) {
+    for (i; i < arr.length - 1; i++) {
+      arr[i] = arr[i + 1];
+    }
+    // remove the hanging item at the end of the array
+    arr.pop();
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback(arr[i])) {
+      removeItem(arr, i);
+    }
+  }
+
+  return arr;
+};
+
+console.log(filterRedux([2, 7, 5, 3, 7], num => num >= 5)); //  [7, 5, 7]
