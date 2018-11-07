428508a266dd256eb2569eacbad57de6b84a6257
Alex <0alexgutierrez@gmail.com>
2018-11-07 10:24:03 -0700

    finished array class

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-const Mem = require("./memory");
+const Mem = require('./memory');
 const memory = new Mem();
 
 class Array {
@@ -14,16 +14,59 @@ class Array {
 
     memory.set(this.ptr + this.length, value);
     this.length++;
+    return this;
   }
 
   _resize(size) {
     const oldPtr = this.ptr;
     this.ptr = memory.allocate(size);
     if (this.ptr === null) {
-      throw new Error("Out of memory");
+      throw new Error('Out of memory');
     }
     memory.copy(this.ptr, oldPtr, this.length);
     memory.free(oldPtr);
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    return memory.get(this.ptr + index);
+  }
+
+  pop() {
+    if (this.length === 0) {
+      throw new Error('Index error');
+    }
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    memory.copy(
+      this.ptr + index,
+      this.ptr + index + 1,
+      this.length - index - 1
+    );
+    this.length--;
   }
 }
 Array.SIZE_RATIO = 3;
@@ -31,5 +74,10 @@ Array.SIZE_RATIO = 3;
 (function main() {
   const arr = new Array();
 
-  arr.push("hello");
+  arr
+    .push(5)
+    .push(3342)
+    .push(55324);
+  arr.remove(2);
+  console.log(arr.length);
 })();
