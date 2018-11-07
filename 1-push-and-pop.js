8c46676d37eb0d8e2cc9cfab1537505c02d5cfde
Morgan Newman <mpnewman95@gmail.com>
2018-11-07 10:46:29 -0800

    Fixes solution 1 to account for capacity changes

--- 1-push-and-pop.js
+++ 1-push-and-pop.js
@@ -1,5 +1,5 @@
-const memory = require('./memory');
-const Array = require('./array');
+const memory = require("./memory");
+const Array = require("./array");
 
 function main() {
   Array.SIZE_RATIO = 3;
@@ -7,26 +7,26 @@ function main() {
   //create an instance of the array class
   let arr = new Array();
 
-  //add an item to the array
   arr.push(5);
+  // length 1, capacity: 3, memory address: 0
+
   arr.push(15);
   arr.push(19);
   arr.push(45);
   arr.push(10);
-  // length 5, capacity: 1024, memory address: 0
+  // length 5, capacity: 12, memory address: 3
 
   arr.pop();
   arr.pop();
   arr.pop();
-  //length: 2, ptr: 0
+  // length 2, capacity: 12, memory address: 3
 
-  // console.log(arr);
-  // console.log(arr.get(0));
   arr.pop();
   arr.pop();
+  // length 0, capacity: 12, memory address: 3
 
-  arr.push('tauhida');
-  console.log(arr.get(0));
-  //result is NaN, our memory only allocates for 64bit floats
+  arr.push("tauhida");
+  // length 1, capacity: 12, memory address: 3
+  //result is NaN, our memory only allocates for 64bit floats. This is a typed arr.
 }
 main();
